ef63fcf11f3e1ef176a4de9b066cb7645e63769e
Alen Oviedo <afoviedola@gmail.com>
2025-04-16 12:23:18 -0500

    Agrego estilos en las fonts

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,12 +6,19 @@
 // }
 
 import Navbar from "../components/Navbar";
+import { Roboto } from "next/font/google";	//Para importar diferentes tipos de letra
 
 export const metadata = {
 	title: "My tienda con Next.js",
 	description: "Esta es la página principal de mi tienda",
 	keywords: "tienda, online, ecommerce"
 }
+
+const roboto = Roboto({
+	weight: ["300", "400", "500", "700"],
+	styles: ["italic", "normal"],
+	subsets: ["latin"]
+})
 
 export default function RootLayout({ children }) {
   return (
@@ -23,7 +30,7 @@ export default function RootLayout({ children }) {
 			<title>My App</title>
 		</head> 
 		*/}
-      	<body>
+      	<body className={ roboto.className }>
 			<Navbar />
 			{children}
 		</body>
